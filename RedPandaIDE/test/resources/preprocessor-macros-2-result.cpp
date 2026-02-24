18de8d888238f39fc345025553da00162512e6ac
Roy Qu <royqh1979@gmail.com>
2026-02-24 09:44:13 +0800

    remove no use test

--- RedPandaIDE/test/resources/preprocessor-macros-2-result.cpp
+++
@@ -1,6 +0,0 @@
-#define EMPTY
-#define SCAN(x)     x
-#define EXAMPLE_()  EXAMPLE
-#define EXAMPLE(n)  EXAMPLE_ EMPTY()(n-1) (n)
-EXAMPLE_()(5-1)(5)
-EXAMPLE_()(5-1-1)(5-1)(5)
--- RedPandaIDE/test/resources/preprocessor-macros-2.cpp
+++
@@ -1,6 +0,0 @@
-#define EMPTY
-#define SCAN(x)     x
-#define EXAMPLE_()  EXAMPLE
-#define EXAMPLE(n)  EXAMPLE_ EMPTY()(n-1) (n)
-EXAMPLE(5)
-SCAN(EXAMPLE(5))
